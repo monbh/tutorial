9540e9173123b1df49c2b3bf28ce1e4d42f6bbb9
monika <monika@codeignition.co>
2014-12-10 13:23:06 +0530

    initial commit

---
+++ .chef/knife.rb
@@ -0,0 +1,7 @@
+log_level                :info
+log_location             STDOUT
+node_name                'monika'
+client_key               '/home/monika/tutorial/.chef/monika.pem'
+validation_client_name   'chef-validator'
+validation_key           '/home/monika/tutorial/.chef/chef-validator.pem'
+chef_server_url          'https://10.21.1.10:443'
